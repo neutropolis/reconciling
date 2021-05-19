ccccd7f2dd13b617a72e5d30337aff3b0caaea8a
Jesús López-González <jesuslopezgonzalez@lavabit.com>
2021-05-19 16:50:37 +0200

    Update Utils.scala

--- src/main/scala/dev/habla/reconciling/Utils.scala
+++ src/main/scala/dev/habla/reconciling/Utils.scala
@@ -7,7 +7,7 @@ given staging.Compiler = staging.Compiler.make(getClass.getClassLoader)
 type Code[A] = Quotes ?=> Expr[A]
 
 def debug[A](expr: Code[A]): Unit =
-  println(s"Output: " + staging.run{ q ?=>
+  println(s"Output: " + staging.run { q ?=>
     println(s"Expression: ${q.show(expr)}")
     expr 
   })
